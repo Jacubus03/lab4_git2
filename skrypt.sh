dd1d85eb3de68f43885219bb8962b7ab1e319de4
Jakub <jakubgorski1212@gmail.com>
2024-05-06 22:59:24 +0200

    Add --log {num}

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+arg=$2
+
 case $1 in
   --date)
     date
     ;;
   --logs)
-    for i in {1..10}
+    if  [ ! $arg]
+    then
+      arg=100
+    fi
+    for (( i=1; i<=$arg; i++ ))
     do
       echo -e "log$i.txt\n$0\n$(date)" > log$i.txt
     done
