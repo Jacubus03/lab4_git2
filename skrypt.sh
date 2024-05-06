c40fb8a4acf74eb0f16cc35194ad3d4f16846725
Jakub <jakubgorski1212@gmail.com>
2024-05-06 22:43:26 +0200

    Add --logs

--- skrypt.sh
+++ skrypt.sh
@@ -4,6 +4,12 @@ case $1 in
   --date)
     date
     ;;
+  --logs)
+    for i in {1..10}
+    do
+      echo -e "log$i.txt\n$0\n$(date)" > log$i.txt
+    done
+    ;;
   *)
     echo "Invalid option: $1"
     ;;
